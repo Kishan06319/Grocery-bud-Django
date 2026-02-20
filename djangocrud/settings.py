3c84553ae342963fbe043130d82c942c845087ac
Kishan khatiwada <KishanKhatiwada44@gmail.com>
2026-02-20 22:08:09 +0545

    Configured DATABASE_URL for Render Postgres

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -77,9 +77,15 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 import dj_database_url
+
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
