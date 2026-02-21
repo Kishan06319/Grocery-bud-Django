a882d6478d2244a649c868bed671291fef13091a
Kishan khatiwada <KishanKhatiwada44@gmail.com>
2026-02-21 22:30:07 +0545

    Added delete feature for grocery items

--- grocery/views.py
+++ grocery/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import GroceryItem
 
-def index(request):
-    items = GroceryItem.objects.all()
-    return render(request, 'grocery/index.html', {'items': items})
+def toggle_completed(request, item_id):
+    if request.method == 'POST':
+        item = get_object_or_404(GroceryItem, id=item_id)
+        item.completed = not item.completed
+        item.save()
+    return redirect('grocery:index')
+
+def delete_item(request, item_id):
+    if request.method == 'POST':
+        item = get_object_or_404(GroceryItem, id=item_id)
+        item.delete()
+    return redirect('grocery:index')
